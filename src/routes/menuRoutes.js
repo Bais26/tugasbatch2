17ebc3621f93fa7ce0ec50fbeb6737539566539e
MBAISYUFANMARDIANSYAH <baisyufan2004@gmail.com>
2023-02-20 21:03:13 +0700

    Restfull ORM-express

---
+++ src/routes/menuRoutes.js
@@ -0,0 +1,18 @@
+const menuRoutes = require('express').Router();
+const menuControllers = require('../controllers/menuControllers');
+// const authMiddleware = require('../middleware')
+const uploaddMiddleware = require('../middleware/uploadMiddleware') 
+const cloudinaryMiddleware = require('../middleware/cloudinaryMiddleware');
+const { searchBytitle } = require('../controllers/menuControllers');
+// const { searchBytitle } = require('../controllers/menuControllers');
+
+
+menuRoutes.post ('/', uploaddMiddleware,
+ cloudinaryMiddleware,
+menuControllers.postDataMenu);
+menuRoutes.get('/', menuControllers.getAllData, searchBytitle ); 
+menuRoutes.delete('/:id', menuControllers.deleteData);
+menuRoutes.put('/:id', menuControllers.updateData);
+menuRoutes.get('/:id', menuControllers.getByid);
+
+module.exports = menuRoutes;
